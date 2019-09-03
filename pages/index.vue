7fa82e62e5e879420d8aa0ffa4f7102864d65e70
wakkihaya <wakkihaya@gmail.com>
2019-09-04 07:11:18 +0900

    top画面の実装

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,31 @@
 <template>
+  <html>
   <body>
- <div class="container">
-  <header>
-    <img src="feedal_logo.jpg">
-  <nav>
-    <ul class="navigation">
-      <li>ミッション</li>
-      <li>サービス</li>
-      <li>ニュース</li>
-      <li>会社情報</li>
-      <li>お問い合わせ</li>
-    </ul>
-  </nav>
-  </header>
+  <div class="container">
+      <header>
+        <img src="feedal_logo.jpg">
+        <nav>
+          <ul class="navigation">
+            <li>ミッション</li>
+              <li>サービス</li>
+              <li>ニュース</li>
+              <li>会社情報</li>
+              <li>お問い合わせ</li>
+          </ul>
+        </nav>
+      </header>
 
+
+
+        <div class="top">
+          <div class="bg"></div>
+            <p>だれもが、<br><br><br>
+            "どんな情報でも”、”気軽に”、”すぐに”<br><br><br>
+            収集できる世界へ</p>
+        </div>
   </div>
   </body>
+  </html>
 </template>
 
 
